1175141929b5958af3059c567dab847df1d651d3
Shravani <Shravani@LAPTOP-U14ME4PN>
2021-08-17 11:07:08 +0530

    Sales class display method added with sum

--- demo1/src/main/java/demo1/Sales1.java
+++ demo1/src/main/java/demo1/Sales1.java
@@ -5,9 +5,13 @@ public class Sales1 {
 	public void display ()
 	{
 	int[] arr= {11,23,45,12,10};
+	int sum=0;
 	for(int num:arr)
+	{
+		sum+=num;
 		System.out.println(num);
-	
+	}
+	System.out.println("summation:"+sum);
 
 }
 	public void print()
