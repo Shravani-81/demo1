fcb5d3b85fa769bbf923ff5fe96b7f1fa3e921e8
Shravani <Shravani@LAPTOP-U14ME4PN>
2021-08-17 14:19:27 +0530

    display method added

--- demo1/src/main/java/demo1/Sales1.java
+++ demo1/src/main/java/demo1/Sales1.java
@@ -4,14 +4,19 @@ public class Sales1 {
 
 	public void display ()
 	{
+	int sum=0;
 	int[] arr= {11,23,45,12,10};
 	
 	for(int i=0;i<arr.length;i++)
+	{
 	
+		 sum+=arr[i];
 		
 		System.out.println(arr[i]);
+	}
 	
 	System.out.println("Array Size:"+arr.length);
+	System.out.println("Summatio:"+sum);
 
 }
 	public void print()
